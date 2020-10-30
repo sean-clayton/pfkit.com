f4fc803e6ec2ccde2a649db97a9f483fed0c7798
Sean Clayton 🎷🐢 <sean@seanclayton.me>
2020-10-30 18:18:05 -0400

    Add some test cases

---
+++ test/pfkit/character_sheet_test.exs
@@ -0,0 +1,15 @@
+defmodule Pfkit.CharacterSheetTest do
+  use ExUnit.Case, async: true
+
+  test "can roll for initiative" do
+
+  end
+
+  test "can determine ability score modifiers" do
+
+  end
+
+  test "can determine proficiency modifier" do
+
+  end
+end
