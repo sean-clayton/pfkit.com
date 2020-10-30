a2251f8686b41c1cdac184cff9c09ce64fdeda95
Sean Clayton 🎷🐢 <sean@seanclayton.me>
2020-10-30 18:12:03 -0400

    Update test file

---
+++ test/pfkit/dice_test.exs
@@ -0,0 +1,39 @@
+defmodule Pfkit.Dicetest do
+  use ExUnit.Case, async: true
+
+  test "can roll a d4" do
+
+  end
+
+  test "can roll a d6" do
+
+  end
+
+  test "can roll a d8" do
+
+  end
+
+  test "can roll a d10" do
+
+  end
+
+  test "can roll a d12" do
+
+  end
+
+  test "can roll a d20" do
+
+  end
+
+  test "can roll a d100" do
+
+  end
+
+  test "can create a dice pool" do
+
+  end
+
+  test "can add dice to a dice pool" do
+
+  end
+end
